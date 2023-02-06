ea1a56c273c63543434e7419e81a4b802c3fe0ef
Almedin Hodzic <almedin.json@gmail.com>
2023-02-06 19:52:52 +0100

    Made auth middleware

---
+++ server/src/api/v1/middlewares/auth/auth.middleware.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+export const authMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const auth = req.headers.authorization;
+  if (auth && auth.startsWith("Bearer")) {
+    const token = auth.slice(7);
+
+    try {
+      const tokenData = jwt.verify(token, process.env.JWT_SECRET!);
+      tokenData as { _id: string; email: string };
+      req.body.tokenData = tokenData;
+      next();
+    } catch (error) {
+      res.status(401).json({ msg: "User can not be authenticated" });
+    }
+  } else {
+    res.status(401).json({ msg: "User can not be authenticated" });
+  }
+};
